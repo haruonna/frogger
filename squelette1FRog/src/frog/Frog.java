5a56ce783cb3faece2cb6b93934d5cba35b4d815
AlexandreDeshayes <74239468+AlexandreDeshayes@users.noreply.github.com>
2020-12-06 11:13:18 +0100

    o

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,48 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
+	private Case myPosition;
 
+	public Frog(Game game){
+		this.game = game;
+		this.myPosition = new Case(13,  0);
+	}
+
+
+	public Case getPosition(){
+	return this.myPosition;
+	}
+
+
+	public Direction getDirection(){
+	return null;
+	}
+
+
+	public void move(Direction key){
+		if (myPosition.ord == 0){
+			if(key.equals(Direction.down)){
+				myPosition.ord = myPosition.ord;
+			}
+		}
+		if (key.equals(Direction.up)){
+			myPosition.ord = myPosition.ord + 1;
+		}
+		if (key.equals(Direction.down)){
+			myPosition.ord = myPosition.ord - 1;
+		}
+		if (key.equals(Direction.right)){
+			myPosition.absc = myPosition.absc + 1;
+		}
+		if (key.equals(Direction.left)){
+			myPosition.absc = myPosition.absc - 1;
+		}
+	}
 
 }
