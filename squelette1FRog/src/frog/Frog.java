b8e37ab2530672170f7c7e9e64e21d806d7461cc
Jess <jessica.imbert@icloud.com>
2020-12-06 11:02:14 +0100

    modif

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,37 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
+    // gérer: ajouter des attributs ?
+	// oui, on veut ses coordonnées : gérer par une case (géré par getCase)
+	// et une direction (dans quel sens pointe la grenouille)
+	private Case myPosition;
 
 
+	public Frog(Game game){
+		this.game = game;
+		this.myPosition = new Case( 0, 0);
+	}
+
+	@Override
+	public Case getPosition(){
+		return myPosition;
+	}
+
+	@Override
+	public Direction getDirection(){
+		//TODO
+		return null;
+	}
+
+	@Override
+	public void move (Direction key){
+		//TODO
+	}
 }
+
